2ac8507af5e0afd82dc9467bb3d78cd302682f4f
Luisa Kirchhof <luisa.kirchhof@kellogg.northwestern.edu>
2023-05-17 01:19:13 +0000

    mid way

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,34 @@
 class PlacesController < ApplicationController
   def index
-    # htlm =  
-    # render :inline => html
-    render :template => "places/index"
+    #find all places
+    @places = Place.all
+    # render :template => "places/index"
   end
+
+  def show
+    # find a Place
+    @place = Place.find_by({"id" => params["id"]})
+    @posts = Post.where({"place_id" => params["id"]})
+  end
+
+  def new
+    @place = Place.new
+  end
+  
+  def create
+    # start with a new Place
+    @place = Place.new
+
+    puts params["place"]
+
+    # assign user-entered form data to Place's columns
+    @place["name"] = params["place"]["name"]
+
+    # save Place row
+    @place.save
+
+    # redirect user
+    redirect_to "/places"
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,26 @@
 class PostsController < ApplicationController
+  
   def index
-    # htlm =  
-    # render :inline => html
-    render :template => "posts/index"
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # start with a new Post
+    @post = Post.new
+    # assign user-entered form data to Post's columns
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["post"]["place_id"] 
+
+    # save Post row
+    @post.save
+    
+    # redirect user
+    redirect_to "/posts"
   end
 end
